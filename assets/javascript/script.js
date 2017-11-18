35fb75259d7f12ca50dceae9ae40f4195d5ba83a
STEVEN TRAN <StevenTran@dhcp-v093-194.mobile.uci.edu>
2017-11-18 13:49:59 -0800

    added some js logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@
 
 
       // $.ajax({
-      // 	url:'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search?q=Goosinator&type=beer&key=9ec4dd555b05addcdc32bc600a2dd1f2&format=json',
+      //	url:'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search?q=Goosinator&type=beer&key=9ec4dd555b05addcdc32bc600a2dd1f2&format=json',
       // 	method: 'GET'
       // }).done(function(response){
       	
@@ -12,15 +12,10 @@
 
       
 
-      // $.ajax({
-      //   url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=ymxM1PfpEkuIniAcXSR6o9tUFCk1Uqo4&api_secret=n6KLZyFDrjsbFycCzxBs-ifyTNuyjZf8&image_file='+img+'&return_attributes=age,gender,smiling,emotion,ethnicity,eyestatus,beauty',
-      //   method: 'POST'
-      // }).done(function(response){
-      //   console.log(response)
-      // })
+      
       $('.btn').on('click', function(){
-        const client = filestack.init('ACvWqWhqT0uESSK94Rojtz');
-        client.pick();
+        // const client = filestack.init('ACvWqWhqT0uESSK94Rojtz');
+        // client.pick();
         
         var fsClient = filestack.init('ACvWqWhqT0uESSK94Rojtz');
         function openPicker() {
@@ -28,12 +23,24 @@
             fromSources:["local_file_system","url","dropbox"],
             accept:["image/*"]
           }).then(function(response) {
+
       // declare this function to handle response
-      handleFilestack(response);
-      
+      console.log(response);
+
+      var img = response.filesUploaded[0].url
+      console.log(img)
+      $.ajax({
+        url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=ymxM1PfpEkuIniAcXSR6o9tUFCk1Uqo4&api_secret=n6KLZyFDrjsbFycCzxBs-ifyTNuyjZf8&image_url='+img+'&return_attributes=age,gender,smiling,emotion,ethnicity,eyestatus,beauty',
+        method: 'POST'
+      }).done(function(results){
+        console.log(results)
+      })
     });
         }
+        openPicker();
       })
+
+
     });
 
 
