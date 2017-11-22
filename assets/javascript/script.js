50ef02160258b6f594bd5cb9ca9a73db6cf2cc89
STEVEN TRAN <StevenTran@dhcp-v110-065.mobile.uci.edu>
2017-11-21 20:49:21 -0800

    added working AJAX

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,31 @@
     $(document).ready(function(){
       $('.parallax').parallax();
 
+      //Testing
+      var zipCode = prompt('What is your zipcode?')
+      
 
       $.ajax({
-        url:'https://maps.googleapis.com/maps/api/geocode/json?address=90504&key=AIzaSyAH9tIVsJmQ37qrU7b3NY3smmAtUmCrgIs',
+        url:'https://maps.googleapis.com/maps/api/geocode/json?address='+zipCode+'&key=AIzaSyAH9tIVsJmQ37qrU7b3NY3smmAtUmCrgIs',
         method: 'GET'
-      }).done(function(zipcode){
-        console.log(zipcode)
+      }).done(function(zip){
+        //console.log(zip)
+        
+        var lat = zip.results[0].geometry.bounds.northeast.lat
+        var lng = zip.results[0].geometry.bounds.northeast.lng
+
+        $.ajax({
+          url:'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search/geo/point?lat='+lat+'&lng='+lng+'&key=9ec4dd555b05addcdc32bc600a2dd1f2&unit=mi&format=json',
+          method: 'GET'
+        }).done(function(response){
+
+          console.log(response)
+        })
       })
 
-      $.ajax({
-      	url:'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search/geo/point?lat=33.8870609&lng=-118.308933&key=9ec4dd555b05addcdc32bc600a2dd1f2&unit=mi&format=json',
-      	method: 'GET'
-      }).done(function(response){
-      	
-      	console.log(response)
-      })
+      
 
-                  
+
       $('.btn').on('click', function(){
         // const client = filestack.init('ACvWqWhqT0uESSK94Rojtz');
         // client.pick();
@@ -28,23 +36,23 @@
           fsClient.pick({
             fromSources:["local_file_system","webcam","url"],
             accept:["image/*"]
-            }).then(function(response) {       
-              console.log(response);
+          }).then(function(response) {       
+            console.log(response);
 
-              var img = response.filesUploaded[0].url
-        
-              $.ajax({
+            var img = response.filesUploaded[0].url
+
+            $.ajax({
               url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=ymxM1PfpEkuIniAcXSR6o9tUFCk1Uqo4&api_secret=n6KLZyFDrjsbFycCzxBs-ifyTNuyjZf8&image_url='+img+'&return_attributes=age,gender,smiling,emotion,ethnicity,eyestatus',
               method: 'POST'
-              }).done(function(results){
-                console.log(results)
+            }).done(function(results){
+              console.log(results)
               
-                var age = results.faces[0].attributes.age.value
-                var happy = Math.round(results.faces[0].attributes.emotion.happiness) 
-                var anger = Math.round(results.faces[0].attributes.emotion.anger)
-                var sad = Math.round(results.faces[0].attributes.emotion.sadness)             
-                var div = $('.card-content')
-                
+              var age = results.faces[0].attributes.age.value
+              var happy = Math.round(results.faces[0].attributes.emotion.happiness) 
+              var anger = Math.round(results.faces[0].attributes.emotion.anger)
+              var sad = Math.round(results.faces[0].attributes.emotion.sadness)             
+              var div = $('.card-content')
+
                 // console.log('happy: '+ happy)
                 // console.log('anger: '+ anger)
                 // console.log('sad: '+ sad)
@@ -61,7 +69,7 @@
                 }
 
               })
-            })
+          })
         }
         openPicker();
       });
