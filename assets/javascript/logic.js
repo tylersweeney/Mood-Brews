7b3448cf203352748434e7bc0d616832ed808448
Jun-Wei Chen <jwchen1259@gmail.com>
2017-11-28 10:54:27 -0800

    Add logic.js, using HTML Geolocation instead of Maps API

---
+++ assets/javascript/logic.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){   
+
+  function getLocation() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(setPosition);
+      }
+  }
+      
+  function setPosition(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+      $.ajax({
+        url:'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search/geo/point?lat='+lat+'&lng='+lng+'&key=9ec4dd555b05addcdc32bc600a2dd1f2&unit=mi&format=json',
+        method: 'GET'
+      }).done(function(response){
+        console.log(response)
+      })
+  }
+
+  getLocation();
+  
+  $('.btn').on('click', function(){        
+    var fsClient = filestack.init('ACvWqWhqT0uESSK94Rojtz');
+        
+    function openPicker() {
+      fsClient.pick({
+        fromSources:["local_file_system","webcam","url"],
+        accept:["image/*"]
+      }).then(function(response) {       
+        console.log(response);
+
+        var img = response.filesUploaded[0].url
+
+        $.ajax({
+          url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=ymxM1PfpEkuIniAcXSR6o9tUFCk1Uqo4&api_secret=n6KLZyFDrjsbFycCzxBs-ifyTNuyjZf8&image_url='+img+'&return_attributes=age,gender,smiling,emotion,ethnicity,eyestatus',
+          method: 'POST'
+        }).done(function(results){
+          console.log(results)
+              
+          var age = results.faces[0].attributes.age.value
+          var happy = Math.round(results.faces[0].attributes.emotion.happiness) 
+          var anger = Math.round(results.faces[0].attributes.emotion.anger)
+          var sad = Math.round(results.faces[0].attributes.emotion.sadness)             
+          var div = $('.card-content')
+
+          if (happy > anger){
+            div.append($('<p>').text('Happy Score: '+ happy))
+            div.append($('<img class="images_div" src="'+img+'">'))
+          } else if (anger > happy){
+            div.append($('<p>').text('Anger Score: '+ anger))
+            div.append($('<img class="images_div" src="'+img+'">'))
+          } else {
+            div.append($('<p>').text('Sad Score: '+ sad))
+            div.append($('<img class="images_div" src="'+img+'">'))
+          }
+
+        })
+      })
+    }
+    openPicker();
+  });
+});
+
+
+
+
+
+
+
+
+
