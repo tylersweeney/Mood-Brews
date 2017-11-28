5f28d1c6ffee44a9e975ce4801fce2539d4f11ee
Jun-Wei Chen <jwchen1259@gmail.com>
2017-11-28 10:09:28 -0800

    Add smooth scrolling

---
+++ assets/javascript/interface.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  // Implement smooth scrolling
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  // Parallax initialization
+  $('.parallax').parallax();
+
+  // Pushpin initialization
+  $('.target').pushpin({
+    top: 0,
+    bottom: 1000,
+    offset: 0
+  });
+
+  $('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+  });
+
+});
+
+
+
+
+
+
+
+
+
