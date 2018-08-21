42a22aa9a1c39467941c7037c1720401d576932c
Tyler Sweeney <tylersweeney@gmail.com>
2018-08-21 11:51:12 -0700

    add uppy file uploader npm and dependencies

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
+const Uppy = require('@uppy/core')
+const XHRUpload = require('@uppy/xhr-upload')
+const DragDrop = require('@uppy/drag-drop')
+
 $(document).ready(function(){
   $('.parallax').parallax();
-
+});
 let beer= "happy"
 
 $.ajax({
@@ -15,51 +19,46 @@ method: 'GET'
     $(".name").append(response.data[i].name)
     $(".abv").append("ABV: " + response.data[i].style.abvMax + "%")
     $(".name").append('<img src="' + response.data[i].labels.medium + '">')
-    // if(Error){
-    //   console.log("error")
-    // continue;
+    if(Error){
+      console.log("error")
+    continue;
   
-  }
-  
-  
-  
-console.log(response)
+  };
+};
 });
 
-
   
 
-  
-  $('.btn').on('click', function(){
-    // const client = filestack.init('ACvWqWhqT0uESSK94Rojtz');
-    // client.pick();
+  // $('.btn').on('click', function(){
+  //   // const client = filestack.init('ACvWqWhqT0uESSK94Rojtz');
+  //   // client.pick();
     
-    var fsClient = filestack.init('ACvWqWhqT0uESSK94Rojtz');
-    function openPicker() {
-      fsClient.pick({
-        fromSources:["local_file_system","url","dropbox"],
-        accept:["image/*"]
-      }).then(function(response) {
+  //   var fsClient = filestack.init('ACvWqWhqT0uESSK94Rojtz');
+  //   function openPicker() {
+  //     fsClient.pick({
+  //       fromSources:["local_file_system","url","dropbox"],
+  //       accept:["image/*"]
+  //     }).then(function(response) {
 
-  // declare this function to handle response
-  console.log(response);
+  // // declare this function to handle response
+  // console.log(response);
 
-  var img = response.filesUploaded[0].url
-  console.log(img)
+  // var img = response.filesUploaded[0].url
+  // console.log(img)
   
-  $.ajax({
-    url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=ymxM1PfpEkuIniAcXSR6o9tUFCk1Uqo4&api_secret=n6KLZyFDrjsbFycCzxBs-ifyTNuyjZf8&image_url='+img+'&return_attributes=age,gender,smiling,emotion,ethnicity,eyestatus,beauty',
-    method: 'POST'
-  }).done(function(results){
-    console.log(results)
-  })
-});
-    }
-    openPicker();
-  })
+//   $.ajax({
+//     url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=ymxM1PfpEkuIniAcXSR6o9tUFCk1Uqo4&api_secret=n6KLZyFDrjsbFycCzxBs-ifyTNuyjZf8&image_url='+img+'&return_attributes=age,gender,smiling,emotion,ethnicity,eyestatus,beauty',
+//     method: 'POST'
+//   }).done(function(results){
+//     console.log(results)
+//   })
+// });
+    
+//     openPicker();
+//   })
 
 
-});
+// });
 
 
 
