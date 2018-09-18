af5ed1a8cd56603b44dec8bee1e58f66b9468a04
Tyler Sweeney <tylersweeney@gmail.com>
2018-09-17 22:08:01 -0700

    image upload works, facial recognition works, beer suggestions work but beers without a label cause an error

--- public/script.js
+++ public/script.js
@@ -1,49 +1,96 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+var output = document.getElementById("output");
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
 $( document ).ready(function() {
   console.log( "ready!" );
 
 
 var widget = uploadcare.Widget('[role=uploadcare-uploader]');
 
-widget.onUploadComplete(function (fileInfo) {
+    widget.onUploadComplete(function (fileInfo) {
 
-  console.log('File name: ', fileInfo.name);
-  console.log('CDN URL: ', fileInfo.cdnUrl);
-var profile = (fileInfo.cdnUrl);  
-$(".profile").append('<img src="' + fileInfo.cdnUrl + '">');
+            console.log('File name: ', fileInfo.name);
+            console.log('CDN URL: ', fileInfo.cdnUrl);
+          var profile = (fileInfo.cdnUrl);  
+          $(".profile").append('<img src="' + fileInfo.cdnUrl + '">');
+          $(".circle").append('<img src="' + fileInfo.cdnUrl + '">');
   
-  // and so on...
 
+            $.ajax({
+              url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=gJZ3nUhkeoCAF0PEtxIPqnpKJqdZ2WCI&api_secret=j6_u1B2jNR4K5BGCIyRBrPHhnoYyQ6Vs&image_url='+profile+'&return_attributes=age,gender,emotion',
+              method: 'POST'
+            }).done(function(response){
+            console.log(response);
+            var beer = "";
+            var detect = (response.faces[0].attributes.emotion);
+            var happiness = (response.faces[0].attributes.emotion.happiness);
+            var sadness = (response.faces[0].attributes.emotion.sadness);
+            var disgust = (response.faces[0].attributes.emotion.disgust);
+            var anger = (response.faces[0].attributes.emotion.anger);
+            var surprise = (response.faces[0].attributes.emotion.surprise);
+            console.log(detect)
+            $(".happiness").append("Happiness: " + happiness)
+            $(".sadness").append("Sadness: " + sadness)
+            $(".disgust").append("Disgust: " + disgust)
+            $(".anger").append("Anger: " + anger)
+            $(".surprise").append("Surprise: " + surprise)
+            // var face = (response);
+            // var obj = JSON.parse(face)
+            // output.innerHTML += emotion;
+            if (happiness > 25){
+              var beer = "happy"
+            } else if (sadness > 25){
+              var beer = "sad"
+            } else if (disgust > 25){
+              var beer = "disgusting"
+            } else if (anger > 25){
+              var beer = "angry"
+            } else if (surprise > 25){
+              var beer = "surprise"
+            }
+            console.log(beer);
+            $.ajax({
+              url:"https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search?q=92627&type=brewery&key=9ec4dd555b05addcdc32bc600a2dd1f2&format=json",
+              method: 'GET'
+              }).done(function(response){
+                console.log(response)
+            $.ajax({
+              url:"https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&hasLabels&key=9ec4dd555b05addcdc32bc600a2dd1f2&format=json",
+              method: 'GET'
+              }).done(function(response){
+                console.log(response)
+                for(let i=0; i<5; i++) {
 
+                  console.log(response.data[i].name)
+                  // console.log(response.data[i].labels.medium)
+                  console.log("ABV: " + response.data[i].style.abvMax + "%")
+                  $(".name").append(response.data[i].name)
+                  $(".abv").append("ABV: " + response.data[i].style.abvMax + "%")
+                  // $(".image").append('<img src="' + response.data[i].labels.medium + '">')
+              };
+              });
+            });
 
-
-
-
-
-$.ajax({
-  url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=gJZ3nUhkeoCAF0PEtxIPqnpKJqdZ2WCI&api_secret=j6_u1B2jNR4K5BGCIyRBrPHhnoYyQ6Vs&image_url='+profile+'&return_landmark=1&return_attributes=age,gender,smiling,emotion,ethnicity,eyestatus,beauty',
-  method: 'POST'
-}).done(function(results){
-  console.log(results)
-})
-});
+    });
 });
 
 // let beer= "happy"
 
-// $.ajax({
-// url:"https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&withLabels&key=9ec4dd555b05addcdc32bc600a2dd1f2&format=json",
-// method: 'GET'
-// }).done(function(response){
-//   let beers = [];
-//   for(let i=0; i<3; i++) {
-//     console.log(response.data[i].name)
-//     console.log(response.data[i].labels.medium)
-//     console.log("ABV: " + response.data[i].style.abvMax + "%")
-//     $(".name").append(response.data[i].name)
-//     $(".abv").append("ABV: " + response.data[i].style.abvMax + "%")
-//     $(".image").append('<img src="' + response.data[i].labels.medium + '">')
-// };
-// });
+
 
 // $.ajax
 
@@ -65,16 +112,10 @@ $.ajax({
   // console.log(img)
   
 
-  //   url: "https://cors-anywhere.herokuapp.com/https://738618318492637:P31Qlc2xmqJmPu6d8hDZsWVcllA@api.cloudinary.com/v1_1/deukkh9yb/resources/image",
-//   method: "GET"
-// }).done(function(response){
-//   console.log(response.resources.image.upload)
 
 
 
 
 
 
-
-
-
+})
