8dd6dd98189157fe129717cb46ee7beee5e81586
Tyler <tylersweeney@gmail.com>
2018-11-16 20:01:13 -0800

    fixed CORS error, now displays Brewdb API responses

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const ejs = require('ejs');
 const path = require('path');
+const cors = require('cors');
 // const keys = require('./config/keys');
 
-
+app.use(cors());
 
 
 //set up view engine
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs');
 
+
+
 // create home route
 app.get('/', function (req, res){
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers");
     res.render('home'); 
  });
 
--- public/script.js
+++ public/script.js
@@ -26,7 +26,7 @@ var widget = uploadcare.Widget('[role=uploadcare-uploader]');
               url:'https://api-us.faceplusplus.com/facepp/v3/detect?api_key=gJZ3nUhkeoCAF0PEtxIPqnpKJqdZ2WCI&api_secret=j6_u1B2jNR4K5BGCIyRBrPHhnoYyQ6Vs&image_url='+profile+'&return_attributes=age,gender,emotion',
               method: 'POST'
             }).done(function(response){
-            console.log(response);
+            // console.log(response);
             var beer = "";
             var detect = (response.faces[0].attributes.emotion);
             var happiness = (response.faces[0].attributes.emotion.happiness);
@@ -40,9 +40,7 @@ var widget = uploadcare.Widget('[role=uploadcare-uploader]');
             $(".disgust").append("Disgust: " + disgust)
             $(".anger").append("Anger: " + anger)
             $(".surprise").append("Surprise: " + surprise)
-            // var face = (response);
-            // var obj = JSON.parse(face)
-            // output.innerHTML += emotion;
+            var face = (response);
             if (happiness > 25){
               var beer = "happy"
             } else if (sadness > 25){
@@ -55,20 +53,24 @@ var widget = uploadcare.Widget('[role=uploadcare-uploader]');
               var beer = "surprise"
             }
             console.log(beer);
-            var beer = "happy";
+            // var obj = JSON.parse(face)
+            // output.innerHTML += emotion;
+
+            // var beer = "happy";
             // $.ajax({
             //   url:"https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search?q=92627&type=brewery&key=9ec4dd555b05addcdc32bc600a2dd1f2&format=json",
             //   method: 'GET'
             //   }).done(function(response){
             //     console.log(response)
             $.ajax({
-              url:"https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=59f2f6316eed4ddaf8ce120c8cd8e2e8",
+              // url:"https://sandbox-api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=59f2f6316eed4ddaf8ce120c8cd8e2e8",
+              url:"https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=9ec4dd555b05addcdc32bc600a2dd1f2&format=json",
               method: 'GET'
               }).done(function(response){
-                console.log(response)
+                
                 for(let i=0; i<1; i++) {
-
-                  // console.log(response.data[i].name)
+                  console.log(response)
+                  console.log(response.data[i].name)
                   // console.log(response.data[i].labels.medium)
                   console.log("ABV: " + response.data[i].style.abvMax + "%")
                   $(".name").append(response.data[i].name)
