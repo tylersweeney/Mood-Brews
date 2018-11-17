f27e1340a98a5185777ca7a20a730d13f072c159
Tyler <tylersweeney@gmail.com>
2018-11-17 14:01:15 -0800

    add different limits to random number generators to match number of beers available in that search

--- public/script.js
+++ public/script.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
     }).done(function (response) {
       //Variable for BreweryDB search
       var beer = "";
+      var beerChoice= "";
       //Assign facial recognition responses to variable
       var detect = (response.faces[0].attributes.emotion);
       var happiness = (response.faces[0].attributes.emotion.happiness);
@@ -34,21 +35,33 @@ $(document).ready(function () {
       //condtional statement to determine strongest emotion from facial recogntion API
       if (happiness > 25) {
         var beer = "happy"
+        var beerChoice = Math.floor(Math.random() * 50) + 1
+        console.log(beerChoice);
         console.log("search is: " + beer);
       } else if (sadness > 25) {
-        var beer = "sad"
+        var beer = "cheer"
+        var beerChoice = Math.floor(Math.random() * 11) + 1
+        console.log(beerChoice);
         console.log("search is: " + beer);
       } else if (disgust > 25) {
-        var beer = "disgusting"
+        var beer = "weird"
+        var beerChoice = Math.floor(Math.random() * 17) + 1
+        console.log(beerChoice);
         console.log("search is: " + beer);
       } else if (anger > 25) {
         var beer = "angry"
+        var beerChoice = Math.floor(Math.random() * 50) + 1
+        console.log(beerChoice);
         console.log("search is: " + beer);
       } else if (surprise > 25) {
-        var beer = "surprise"
+        var beer = "shock"
+        var beerChoice = Math.floor(Math.random() * 50) + 1
+        console.log(beerChoice);
         console.log("search is: " + beer);
       } else {
         var beer = "robot"
+        var beerChoice = Math.floor(Math.random() * 50) + 1
+        console.log(beerChoice);
         console.log("What is Emotion?");
       }
 
@@ -57,9 +70,6 @@ $(document).ready(function () {
         url: "https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/search?q=" + beer + "&type=beer&key=9ec4dd555b05addcdc32bc600a2dd1f2&format=json",
         method: 'GET'
       }).done(function (response) {
-
-        // Random number so user doesn't always get same top result
-        var beerChoice = Math.floor(Math.random() * 50) + 1
         console.log(response.data[beerChoice].name)
         // console.log(response.data[i].labels.medium)
         console.log("ABV: " + response.data[beerChoice].style.abvMax + "%")
