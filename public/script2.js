f31fdc3911844f485106854685fcab51ad2c9514
Tyler <tylersweeney@gmail.com>
2018-11-16 21:53:57 -0800

    add google fonts, css more wrangled

--- public/script2.js
+++
@@ -1,5 +0,0 @@
-app.all('.endpoint2', function (req, res){
-    res.header("Access-Control-Allow-Origin", "https://localhost:3000");
-    res.header("Access-Control-Allow-Headers");
-    res.send('Hello from Endpoint2!')
-});
