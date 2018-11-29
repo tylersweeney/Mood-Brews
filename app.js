44412055793fa17c856c0874918744c7a4c9585c
Tyler <tylersweeney@gmail.com>
2018-11-28 17:30:54 -0800

    add routes

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ app.use(cors());
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs');
 
-
-
 // create home route
 app.get('/', function (req, res){
     res.render('home'); 
